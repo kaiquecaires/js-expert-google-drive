4864fc1bf80b43c94fe8a00df145ec7bce78ebc7
Kaique Caires <kaique.caires@yahoo.com>
2021-09-06 13:42:21 -0300

    given method GET it should choose get route

--- gdrive-webapi/test/unit/routes.test.js
+++ gdrive-webapi/test/unit/routes.test.js
@@ -1,7 +1,7 @@
 import { describe, test, expect, jest } from '@jest/globals'
 import Routes from '../../src/routes'
 
-describe('#Routes test suid', () => {
+describe('#Routes test suit', () => {
   describe('#setSocketInstance', () => {
     test('setSocket should store io instance', () => {
       const routes = new Routes()
@@ -13,65 +13,74 @@ describe('#Routes test suid', () => {
       routes.setSocketInstance(ioObj)
       expect(routes.io).toStrictEqual(ioObj)
     })
+  })
 
-    describe('#handler', () => {
-      const defaultParams = {
-        request: {
-          headers: {
-            'Content-Type': 'multipart/form-data'
-          },
-          method: ''
+  describe('#handler', () => {
+    const defaultParams = {
+      request: {
+        headers: {
+          'Content-Type': 'multipart/form-data'
         },
-        response: {
-          setHeader: jest.fn(),
-          writeHead: jest.fn(),
-          end: jest.fn()
-        },
-        values: () => Object.values(defaultParams)
+        method: ''
+      },
+      response: {
+        setHeader: jest.fn(),
+        writeHead: jest.fn(),
+        end: jest.fn()
+      },
+      values: () => Object.values(defaultParams)
+    }
+
+    test('give an inexistent route it should choose default route', async () => {
+      const routes = new Routes()
+      const params = {
+        ...defaultParams
       }
+      params.request.method = 'inexistent'
+      await routes.handler(...params.values())
+      expect(params.response.end).toHaveBeenCalledWith('hello world')
+    })
 
-      test('give an inexistent route it should choose default route', async () => {
-        const routes = new Routes()
-        const params = {
-          ...defaultParams
-        }
-        params.request.method = 'inexistent'
-        await routes.handler(...params.values())
-        expect(params.response.end).toHaveBeenCalledWith('hello world')
-      })
+    test('it should set any request with CORS enabled', async () => {
+      const routes = new Routes()
+      const params = {
+        ...defaultParams
+      }
+      await routes.handler(...params.values())
+      expect(params.response.setHeader).toHaveBeenCalledWith('Access-Control-Allow-Orgin', '*')
+    })
 
-      test('it should set any request with CORS enabled', async () => {
-        const routes = new Routes()
-        const params = {
-          ...defaultParams
-        }
-        await routes.handler(...params.values())
-        expect(params.response.setHeader).toHaveBeenCalledWith('Access-Control-Allow-Orgin', '*')
-      })
+    test('given method OPTIONS it should choose options route', async () => {
+      const routes = new Routes()
+      const params = {
+        ...defaultParams
+      }
+      params.request.method = 'OPTIONS'
+      await routes.handler(...params.values())
+      expect(params.response.writeHead).toHaveBeenCalledWith(204)
+      expect(params.response.end).toHaveBeenCalled()
+    })
 
-      test('given method OPTIONS it should choose options route', async () => {
-        const routes = new Routes()
-        const params = {
-          ...defaultParams
-        }
-        params.request.method = 'OPTIONS'
-        await routes.handler(...params.values())
-        expect(params.response.writeHead).toHaveBeenCalledWith(204)
-        expect(params.response.end).toHaveBeenCalled()
-      })
+    test('given method POST it should choose post route', async () => {
+      const routes = new Routes()
+      const params = {
+        ...defaultParams
+      }
+      params.request.method = 'POST'
+      jest.spyOn(routes, routes.post.name).mockResolvedValue()
+      await routes.handler(...params.values())
+      expect(routes.post).toHaveBeenCalled()
+    })
 
-      test('given method POST it should choose post route', async () => {
-        const routes = new Routes()
-        const params = {
-          ...defaultParams
-        }
-        params.request.method = 'POST'
-        jest.spyOn(routes, routes.post.name).mockResolvedValue()
-        await routes.handler(...params.values())
-        expect(routes.post).toHaveBeenCalled()
-      })
-
-      test.todo('given method GET it should choose get route')
+    test('given method GET it should choose get route', async () => {
+      const routes = new Routes()
+      const params = {
+        ...defaultParams
+      }
+      params.request.method = 'GET'
+      jest.spyOn(routes, routes.get.name).mockResolvedValue()
+      await routes.handler(...params.values())
+      expect(routes.get).toHaveBeenCalled()
     })
   })
 })
